2e152d261a10615556c21aefe6d94fb54f9e87c9
Catherine Cheng <cat.cheng@gmail.com>
2016-04-18 22:41:57 +0000

    added data

--- server.js
+++ server.js
@@ -21,12 +21,18 @@ var schools = [{
   id: 1,
   imageurl: "https://upload.wikimedia.org/wikipedia/en/thumb/4/44/MIT_Seal.svg/1024px-MIT_Seal.svg.png",
   collegename: "Massachusetts Institute of Technology",
-  duedate: "Janurary 1st, 2017"
+  duedate: {
+    ed_due_date: "NONE",
+    due_date: "January 1st, 2016"
+  }
 }, {
   id: 2,
   imageurl:"http://sadermedia.com/wp-content/uploads/2015/09/ut-logo.png",
   collegename: "University of Texas",
-  duedate: "December 25th, 2016"
+  duedate: {
+    ed_due_date: "December 25th, 2016",
+    due_date: "January 1st, 2016"
+  }
 }];
 
 router.get('/', function (req,res) {
@@ -41,13 +47,19 @@ router.get('/random', function (req, res) {
     
     quote: chance.pickone(['Good job', 'Keep going', 'Don\'t worry, you\'ll get there', 'Winners never quit, and quitters never win',
     'You can do this!', 'Think of a puppy wearing sunglasses', 'Remember that person who gave up? Neither does anyone else', 
-    'Every wrong attempt discarded is a step forward', 'Sometimes adversity is what you need to face in order to become successful'
+    'Every wrong attempt discarded is a step forward', 'Sometimes adversity is what you need to face in order to become successful', 'JUST DO IT',
+    'Don\'t stop  when you are tired, stop when you are done'
     ]),
     
-    author: "inspirational quote"
+    author: "Inspirational Quote",
+    
+    gif: chance.pickone(["stressed1.gif", "stressed2.gif", "stressed3.gif", "stressed4.gif", "stressed5.gif"])
   });
 });
 
+router.get("/college/mit", function(req, res) {
+  res.render("")
+})
 
 
 

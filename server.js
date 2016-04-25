f4e8e9c657a7932abb44e75ce1ea5789ded7b305
aafia <aafubbaia@gmail.com>
2016-04-25 21:47:20 +0000

    merge

--- server.js
+++ server.js
@@ -21,15 +21,13 @@ var schools = [{
   id: 1,
   imageurl: "https://upload.wikimedia.org/wikipedia/en/thumb/4/44/MIT_Seal.svg/1024px-MIT_Seal.svg.png",
   collegename: "Massachusetts Institute of Technology",
-<<<<<<< HEAD
-  duedate: "January 1st, 2017"
-=======
+  duedate: "January 1st, 2017",
   duedate: {
     ed_due_date: "NONE",
     due_date: "January 1st, 2016"
   }
->>>>>>> 2e152d261a10615556c21aefe6d94fb54f9e87c9
-}, {
+}, 
+{
   id: 2,
   imageurl:"http://sadermedia.com/wp-content/uploads/2015/09/ut-logo.png",
   collegename: "University of Texas",
@@ -49,14 +47,6 @@ router.get('/', function (req,res) {
 router.get('/random', function (req, res) {
   res.render('stressed.hbs', {
     
-<<<<<<< HEAD
-    quote: chance.pickone(['Good job.', 'Keep going.', 'Don\'t worry, you\'ll get there.', 'Winners never quit, and quitters never win.',
-    'You can do this.', 'Think of a puppy wearing sunglasses.', 'Remember that person who gave up? Neither does anyone else.', 
-    'Every wrong attempt discarded is a step forward.', 'Sometimes adversity is what you need to face in order to become successful.', 
-    'The advancement and diffusion of knowledge is the only guardian of true liberty.' ]),
-    
-    author: "Inspirational Quote"
-=======
     quote: chance.pickone(['Good job', 'Keep going', 'Don\'t worry, you\'ll get there', 'Winners never quit, and quitters never win',
     'You can do this!', 'Think of a puppy wearing sunglasses', 'Remember that person who gave up? Neither does anyone else', 
     'Every wrong attempt discarded is a step forward', 'Sometimes adversity is what you need to face in order to become successful', 'JUST DO IT',
@@ -66,7 +56,6 @@ router.get('/random', function (req, res) {
     author: "Inspirational Quote",
     
     gif: chance.pickone(["stressed1.gif", "stressed2.gif", "stressed3.gif", "stressed4.gif", "stressed5.gif"])
->>>>>>> 2e152d261a10615556c21aefe6d94fb54f9e87c9
   });
 });
 
